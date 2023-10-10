e7d038cc2a0eba576bbe7fcfe6e2ed382155a238
simofatt <mohamedfatehialhabib@gmail.com>
2023-10-10 23:56:27 +0100

    FirstOne

---
+++ DataStructure/Arrays/Arrays.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure.Arrays
+{
+    public static class ArrayLibrary
+    {
+
+        // USING LINQ
+        public static int[] SwapArray(int[] arr , int elementToMove)
+        {
+            var indexOfTheElementToMove = Array.IndexOf(arr, elementToMove);    
+            var newArray = arr.Where((value,index) => index !=indexOfTheElementToMove).ToArray();
+            newArray = newArray.Concat(new[] {elementToMove}).ToArray(); 
+            return newArray ;
+        }
+        //USING LINKED LIST 
+        public static int[] SwapArraysUsingLinkedList(int[] arr, int elementToMove) { 
+            
+            LinkedList<int> list = new LinkedList<int>(arr);   
+            list.AddLast(elementToMove);
+            list.Remove(elementToMove);
+            
+            return list.ToArray();
+
+        
+        }
+    }
+}
---
+++ LeetCode/Program.cs
@@ -0,0 +1,33 @@
+﻿using LeetCode.Algorithms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode {
+    class Program {
+        public static void Main(string[] args)
+        {
+
+            //LINKED LIST : [2,1,3,5,4,6,7]
+
+            var array =  WinnerOfAnArrayGame.GetWinner(new[] { 1, 25, 35, 42, 68, 70},1); 
+            Console.WriteLine(string.Join(" , " , array));
+
+
+      
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
