1e9456ca025e9f975672b4e0b1fe27943d9a9239
simofatt <mohamedfatehialhabib@gmail.com>
2023-11-23 00:50:50 +0100

    Day8

--- Algorithms/FindLargest.cs
+++ Algorithms/FindLargest.cs
@@ -11,48 +11,66 @@ namespace LeetCode.Algorithms
     {
 
         static Dictionary<string, List<string>> graph = new Dictionary<string, List<string>>();
-        static HashSet<string> visited = new HashSet<string>();   
-        static List<string> findLargest = new List<string>();   
+        static HashSet<string> visited = new HashSet<string>();
+        static List<string> findLargest = new List<string>();
+        static string curr = "";
+
 
         public static string LargestNumber(int[] nums)
         {
-            ToGraph(nums) ; 
+            ToGraph(nums);
 
-            
-            foreach(var item in graph)
-            {
-                if(recurssive(item)) 
-            }
 
-            
-
-            return ""; 
-            }
-
-        public static bool recurssive( string node,int[] nums,string current) 
-        {
-
-            /*[10,2,0] */
-
-            
-
-            if (nums.Length == current.Length) return true;
-
-            foreach(var neighboor in graph[node])
-            {
-
-                if(!visited.Contains(neighboor))
+            foreach (var item in graph)
+            { 
+                if (recurssive(item.Key, nums))
                 {
-                    visited.Add(neighboor);
-                    current += neighboor;  
-                    if (recurssive(neighboor, nums,current)) return true;
+                    //visited.Remove(item.Key);
+                    curr = item.Key;
+                    findLargest.Add(curr);
                 }
             }
 
 
-            return false;
-         
+
+            return "";
+        }
+
+        public static bool recurssive(string node, int[] nums)
+        {
+
+            /*[10,2,0]  */
+
+            Queue<string> queue = new Queue<string>();
+            queue.Enqueue(node);
+
+
+
+
+            while (queue.Count > 0)
+            {
+                if (nums.Length == visited.Count+1) return true;
+
+                var deque = queue.Dequeue();
+                visited.Remove(node);
+
+
+                foreach (var neighboor in graph[deque])
+                {
+
+                    if (!visited.Contains(neighboor))
+                    {
+                        visited.Add(neighboor);
+                        queue.Enqueue(neighboor);
+
+                    }
+                }
+
             }
+
+            return false;
+
+        }
 
         public static void ToGraph(int[] nums)
         {
@@ -66,7 +84,7 @@ namespace LeetCode.Algorithms
                     var to = nums[i].ToString();
                     if (!graph.ContainsKey(to)) graph[to] = new List<string>();
 
-                   if(i !=j) graph[to].Add(nums[j].ToString());
+                    if (i != j) graph[to].Add(nums[j].ToString());
 
                 }
 
@@ -74,9 +92,12 @@ namespace LeetCode.Algorithms
 
 
         }
-    }
-     
 
-  
-    
+
+
+
+
+
+
+    }
 }
