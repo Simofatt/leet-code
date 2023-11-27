40f60090242a2a9ad94342e9c005f07a24f19798
simofatt <mohamedfatehialhabib@gmail.com>
2023-11-27 21:13:36 +0100

    StringCompression

---
+++ Algorithms/Sorting/stringCompression.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Algorithms.Sorting
+{
+    public static  class stringCompression
+    {
+
+        public static int Compress(char[] chars)
+        {
+            HashSet<char> visited = new HashSet<char>(); 
+            ArrayList ints = new ArrayList();
+            for(var i=0; i<chars.Length; i++)
+            {
+                var currentChar = chars[i]; 
+            
+
+                var count = 1; 
+                if (!visited.Contains(currentChar))
+                {
+                    for (var j = 0; j < chars.Length; j++)
+                    {
+                        if (i != j)
+                        {
+                            if (currentChar == chars[j]) chars[i+1] = count++; 
+                        }
+                    }
+                    visited.Add(currentChar);
+                    ints.Add(currentChar);
+                    ints.Add(count); 
+                }
+            }
+          
+            return ints.Count; 
+        }
+    }
+}
