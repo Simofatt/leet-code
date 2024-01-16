a1008c33566280f3b37c9eea300b66aa50d148f8
simofatt <mohamedfatehialhabib@gmail.com>
2024-01-16 23:02:38 +0100

    longestCommonPrefix

---
+++ Java/src/main/java/org/main/Main.java
@@ -0,0 +1,13 @@
+package org.main;
+
+import org.algorithms.arrays.commonPrefix;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        commonPrefix.LongestCommonPrefix(new String[] {"flower","flow","flight"}) ;
+    }
+
+
+}
