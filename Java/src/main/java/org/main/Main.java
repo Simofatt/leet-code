8a0422192a19166d2e53aaf26bff59fd5b94e84a
simofatt <mohamedfatehialhabib@gmail.com>
2024-01-29 20:00:00 +0100

    main

--- Java/src/main/java/org/main/Main.java
+++ Java/src/main/java/org/main/Main.java
@@ -4,7 +4,7 @@ import org.algorithms.arrays.commonPrefix;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Longest commmon prefix");
 
         commonPrefix.longestCommonPrefix(new String[] {"flower","flow","flight"}) ;
     }
